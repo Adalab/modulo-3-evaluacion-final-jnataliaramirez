4fb0e5f56842e965e670b3617d19410cd98456c6
Natalia Ramirez <jnataliaramirez@gmail.com>
2021-10-04 18:37:38 +0200

    Add icons for status

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,6 +1,16 @@
 import '../styles/components/CharacterDetail.scss'
 
 function CharacterDetail (props) {
+  const renderDeadOrAlive = (statusCharacter) => {
+    if (statusCharacter === 'Dead') {
+      return <p><span>Status:</span> <i class="fas fa-skull-crossbones"></i> Dead </p>
+    } else if (statusCharacter === 'Alive') {
+      return <p><span>Status:</span> <i class="fas fa-heart"></i> Alive </p>
+    } else if (statusCharacter === 'unknown') {
+      return <p><span>Status:</span> <i class="fa fa-question" aria-hidden="true"></i> Unknown </p>
+    }
+  }
+
   if (props.dataCharacter === undefined) {
     return (
       <section>
@@ -12,10 +22,10 @@ function CharacterDetail (props) {
     <article>
       <img src={props.dataCharacter.image} alt={`Imagen de ${props.dataCharacter.name}`} />
       <h2>{props.dataCharacter.name} </h2>
-      <p><span>Status:</span> {props.dataCharacter.status} </p>
       <p><span>Species:</span> {props.dataCharacter.species} </p>
       <p><span>Origin:</span> {props.dataCharacter.origin} </p>
       <p><span>Episodes:</span> {props.dataCharacter.episodes}</p>
+      {renderDeadOrAlive(props.dataCharacter.status)}
     </article>
   );
 } 
